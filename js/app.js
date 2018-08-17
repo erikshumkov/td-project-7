d2aa508fd8a28b11e205fb1bfa989e1da62e5fc9
erikshumkov <erikshumkov@gmail.com>
2018-08-17 13:35:14 +0200

    DRY
    Made app.js DRY.
    
    Deleted the hearts functionality and made it DRY.
    
    Added and changed comments.

--- js/app.js
+++ js/app.js
@@ -3,15 +3,16 @@ document.addEventListener('DOMContentLoaded', () => {
   let missed = 0; // Wrong guess count.
   const keyboard = document.querySelector("#qwerty");
   const phrase = document.querySelector("#phrase");
-  const resetButton = document.querySelector(".btn__reset");
+  const resetButton = document.querySelector(".btn__reset"); // Starts the game.
   const overlay = document.querySelector("#overlay");
   const btnRetry = document.createElement("button"); // Button added to the win or lose screen.
   const createSpan = document.createElement("span");
-  const tries = document.querySelectorAll(".tries");
+  const imgs = document.querySelectorAll("img"); // Selects the hearts.
+  let hearts = imgs.length - 1; // Hearts counter.
   let letterFound;
   let correctLetter = []; // Correct guesses for each letter stored in this array.
 
-  // Array for the phrases to guess
+  // Array for the phrases to guess.
   const phrases = [
     'Team Treehouse',
     'Cascading Style Sheets',
@@ -20,9 +21,8 @@ document.addEventListener('DOMContentLoaded', () => {
     'JavaScript and the DOM'
   ];
 
-  // Add any array with phrases to this function
-  // after function is run, the chosen phrase from the array will be deleted
-  // and cannot be used again.
+  // Add any array with phrases to this function.
+  // After function is run, the chosen phrase from the array will be deleted.
   const getRandomPhraseAsArray = arr => {
       let randomNumber = Math.floor(Math.random() * arr.length);
       let newPhrase = arr[randomNumber];
@@ -39,12 +39,12 @@ document.addEventListener('DOMContentLoaded', () => {
         let li = document.createElement("li");
         li.textContent = arr[i];
 
-        // every character get in a LI
-        // append list item to #phrase ul
+        // Every character get in a LI.
+        // Append list item to #phrase ul.
         ul.appendChild(li);
 
-        // if array item is not an empty string, add class name letter.
-        // otherwise create space between the words in phrase.
+        // If array item is not an empty string, add class name letter.
+        // Otherwise create space between the words in the phrase.
         if (arr[i] !== " ") {
           li.className = "letter";
         } else {
@@ -53,8 +53,8 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   }
 
-  // checkLetter to validate if letter is in the phrase display.
-  // if it is, letter in phrase will be displayed with "show"-className.
+  // CheckLetter to validate if letter is in the phrase display.
+  // If it is, letter in phrase will be displayed with "show"-className.
   const checkLetter = letter => {
     const letters = document.querySelectorAll(".letter");
     let letterSaved = letter;
@@ -76,12 +76,12 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
 
-  // check each time player guesses a letter
+  // Check each time player guesses a letter.
 
-  // if number of letters with "show"-class = letters with class "letters"
-  // show overlay win
+  // If number of letters with "show"-class is equal to letters with class "letters"
+  // show win overlay.
 
-  // if misses is greater than or equal to 5, show lose overlay
+  // If misses is greater than or equal to 5, show lose overlay.
   const checkWin = () => {
     const h2 = document.querySelectorAll("h2")[0];
     const link = overlay.querySelector("a");
@@ -90,9 +90,9 @@ document.addEventListener('DOMContentLoaded', () => {
     btnRetry.textContent = "Play Again";
     btnRetry.className = "btn__retry";
 
-    // Loss
+    // Loss.
     if ( missed === 5 ) {
-      // Wait for last heart to disappear
+      // Wait for last heart to disappear.
       setTimeout( () => {
       for (i = 0; i < letters.length; i++) {
         letters[i].className = "letter show";
@@ -104,12 +104,12 @@ document.addEventListener('DOMContentLoaded', () => {
       createSpan.textContent = "You didn't make it this time, but you can try again!";
       overlay.appendChild(btnRetry);
       overlay.style.display = "flex";
-      }, 1800);
+      }, 800);
     }
 
-    // Win
+    // Win.
     if ( letters.length === correctLetter.length ) {
-      // Wait for last letter to show
+      // Wait for last letter to show.
       setTimeout( () => {
       for (i = 0; i < letters.length; i++) {
         letters[i].className = "letter show";
@@ -121,7 +121,7 @@ document.addEventListener('DOMContentLoaded', () => {
       createSpan.textContent = "Play one more time?";
       overlay.appendChild(btnRetry);
       overlay.style.display = "flex";
-      }, 1800);
+      }, 800);
     }
   }
 
@@ -130,13 +130,13 @@ document.addEventListener('DOMContentLoaded', () => {
     overlay.style.display = "none";
   });
 
-  // Get the random phrase from phrases-array, then passing it to addPhraseToDisplay
+  // Get the random phrase from phrases-array, then passing it to addPhraseToDisplay.
   let phraseArray = getRandomPhraseAsArray(phrases);
 
   // Adding a Phrase to the game when overlay is gone.
   addPhraseToDisplay(phraseArray);
 
-  // Add click event listener to #qwerty keyboard
+  // Add click event listener to #qwerty keyboard.
   keyboard.addEventListener("click", (event) => {
     if(event.target.tagName == "BUTTON") {
       let selectedButton = event.target;
@@ -145,28 +145,14 @@ document.addEventListener('DOMContentLoaded', () => {
       letterFound = selectedButton.textContent;
       checkLetter(letterFound);
 
-      // Write a statement to check the letterFound variable
-      // If value is equal to null remove one of the tries from scoreboard
-      // missed variable to store score
-      // If wrong letter, missed gets +1
+      // Write a statement to check the letterFound variable.
+      // If value is equal to null, remove one of the tries from scoreboard.
+      // If wrong letter, missed gets +1 and one heart disappear.
       if (letterFound === null) {
         selectedButton.className = "wrongchoice";
         missed++;
-        if( missed === 1 ) {
-          tries[4].firstChild.setAttribute("src", "images/lostHeart.png");
-        }
-        if( missed === 2 ) {
-          tries[3].firstChild.setAttribute("src", "images/lostHeart.png");
-        }
-        if( missed === 3 ) {
-          tries[2].firstChild.setAttribute("src", "images/lostHeart.png");
-        }
-        if( missed === 4 ) {
-          tries[1].firstChild.setAttribute("src", "images/lostHeart.png");
-        }
-        if( missed === 5 ) {
-          tries[0].firstChild.setAttribute("src", "images/lostHeart.png");
-        }
+        imgs[hearts].setAttribute("src", "images/lostHeart.png");
+        hearts--;
       }
       checkWin();
     }
@@ -175,7 +161,6 @@ document.addEventListener('DOMContentLoaded', () => {
   // Button (btnRetry) to replay the game.
   btnRetry.addEventListener("click", () => {
     const ul = phrase.querySelector("ul");
-    const createUl = document.createElement("ul");
     const phraseLi = ul.querySelectorAll('li');
     const clearKeyboard = keyboard.querySelectorAll("button");
 
@@ -186,15 +171,17 @@ document.addEventListener('DOMContentLoaded', () => {
       }
       // Makes the grey hearts blue again.
       if (missed > 0) {
-        for(i = 0; i < tries.length; i++) {
-          tries[i].firstChild.setAttribute("src", "images/liveHeart.png");
+        for(i = 0; i < imgs.length; i++) {
+          imgs[i].setAttribute("src", "images/liveHeart.png");
         }
       }
-      // Removes all the correct letters from correctLetter.
+      // Removes all the correct letters from array.
       correctLetter = [];
-      // Resets number of misses to 0
+      // Resets number of misses to 0.
       missed = 0;
       letterFound = "";
+      // Resets hearts counter.
+      hearts = imgs.length - 1;
       // Removes old phrase from display, so a new one can be added.
       for(i = 0; i < phraseLi.length; i++) {
         ul.removeChild(phraseLi[i]);
